0825b76478f74228731d1429db944e2efd1a8128
Yaryna Pechenenko <pechenenko.pn@ucu.edu.ua>
2024-11-24 13:04:54 +0200

    add visualize_communities function

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import argparse
-import numpy
 import pandas as pd
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 ######## ------------------------ ########
@@ -27,7 +28,44 @@ def readfile(filename):
     
     return adj_matrix_pretty
 
+
+
+
+def visualize_communities(G, communities):
+    """
+    Visualize graph with community coloring
+    """
+    plt.figure(figsize=(12, 8))
+    
+    # Create color map
+    color_palette = plt.cm.get_cmap('tab20')
+    community_colors = {}
+    for i, comm in enumerate(communities):
+        for node in comm:
+            community_colors[node] = color_palette(i % 20)
+    
+    # Draw graph
+    pos = nx.spring_layout(G, seed=42)
+    nx.draw_networkx_nodes(
+        G,
+        pos,
+        node_color=[community_colors[node] for node in G.nodes()],
+        node_size=300
+    )
+    nx.draw_networkx_edges(G, pos, alpha=0.5)
+    nx.draw_networkx_labels(G, pos)
+    
+    plt.title("Community Structure Visualization")
+    plt.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+
+
+
+
 if __name__ == "__main__":
     data = readfile("data.csv")
 
     print(data)
+
